dba6e880a8cf26a45016df9d04267114fc929b3e
Grey Pearson <grey.pearson@lvt.com>
2024-05-10 10:54:27 -0600

    game now picks up when game is over but does not stop player from making more moves, and does not know if opponent or player has won, opponent can make moves now also

--- tIcTacToeNotOOP.py
+++ tIcTacToeNotOOP.py
@@ -40,6 +40,7 @@ def clicked(r, c):
         tracking_board[r][c] = "X"
 
     check_for_win()
+    opponent_move()
 
 
 def check_for_win():
@@ -47,28 +48,60 @@ def check_for_win():
 
     for tiles in range(board_size):
 
-        if tracking_board[tiles][0] == tracking_board[tiles][1] == tracking_board[tiles][2] != 0:
+        if (
+            tracking_board[tiles][0]
+            == tracking_board[tiles][1]
+            == tracking_board[tiles][2]
+            != 0
+        ):
             stop_game = True
-            print('game over condition 1')
+            print("game over condition 1")
             break
-        
-        if tracking_board[0][tiles] == tracking_board[1][tiles] == tracking_board[2][tiles] != 0:
+
+        if (
+            tracking_board[0][tiles]
+            == tracking_board[1][tiles]
+            == tracking_board[2][tiles]
+            != 0
+        ):
             stop_game = True
-            print('game over, condition 2')
+            print("game over, condition 2")
             break
-        
+
         if tracking_board[0][0] == tracking_board[1][1] == tracking_board[2][2] != 0:
             stop_game = True
-            print('game over condition 2')
-        
+            print("game over condition 2")
+            break
+
         if tracking_board[0][2] == tracking_board[1][1] == tracking_board[2][0] != 0:
             stop_game = True
-            print('game over condition 3')
-        
+            print("game over condition 3")
+            break
+
+    no_symbol = 0
     # this is to verify a tie, should check for tie some better way in a seperate function prolly
     for tiles_x in range(board_size):
-        for tiles_x in range(board_size):
-            if tracking_board[tile]
+        for tiles_y in range(board_size):
+            if tracking_board[tiles_x][tiles_y] == 0:
+                no_symbol += 1
+    if no_symbol == 0:
+        stop_game = True
+        print("game over condition 4")
+
+
+def opponent_move():
+    found_empty_tile = True
+
+    while found_empty_tile:
+        x_tile = random.randint(0, 2)
+        y_tile = random.randint(0, 2)
+
+        if tracking_board[x_tile][y_tile] == 0:
+            button_board[x_tile][y_tile].configure(text="O")
+            tracking_board[x_tile][y_tile] = "O"
+            break
+    check_for_win()
+
 
 # print(tracking_board)
 # print(button_board)
