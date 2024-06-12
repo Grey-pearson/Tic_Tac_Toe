6920cd8fc6320e8a2019357204546640e75d125e
root <root@DESKTOP-473EBOO>
2024-06-12 16:33:31 -0600

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-root = tk.TK()
+root = tk.Tk()
 
 button1 = ttk.Button(root, text="button 1", bootsyle=SUCCESS)
 button1.pack(side=LEFT, padx=5, pady=10)
