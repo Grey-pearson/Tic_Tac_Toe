abfe78e96f8e1fdd3584451d11fd22a525df0e65
root <root@DESKTOP-473EBOO>
2024-06-12 16:28:38 -0600

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -2,6 +2,13 @@ import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-# board = Board()
+root = ttk.Window(themename="solar")
+root.title("VMS-inator")
 
-# board.create_board()
+TDC = ttk.Entry(bootstyle="success").pack(side=TOP, padx=5, pady=10)
+right_cam = ttk.Checkbutton(bootstyle="success").pack()
+center_cam = ttk.Checkbutton(bootstyle="success").pack()
+left_cam = ttk.Checkbutton(bootstyle="success").pack()
+
+
+root.mainloop()
