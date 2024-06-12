add2a13a59b783ae51786dc96073f4990f2feae1
root <root@DESKTOP-473EBOO>
2024-06-12 16:44:09 -0600

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+from Board import *
 
 root = tk.Tk()
 
-button1 = ttk.Button(root, text="button 1", bootstyle=SUCCESS)
-button1.pack(side=LEFT, padx=5, pady=10)
+board = Board()
 
-
+board.create_board()
 
 root.mainloop()
