221373f2013d2517ad3b0de86cdf733896c43513
root <root@DESKTOP-473EBOO>
2024-06-12 16:36:56 -0600

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -7,7 +7,6 @@ root = tk.Tk()
 button1 = ttk.Button(root, text="button 1", bootstyle=SUCCESS)
 button1.pack(side=LEFT, padx=5, pady=10)
 
-button2 = ttk.Button(root, text="button 2", bootstyle=INFO)
-button2.pack(side=RIGHT, padx=5, pady=10)
+
 
 root.mainloop()
