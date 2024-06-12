d3e35a9013188d0989d0d041fca092afb37c5cbf
root <root@DESKTOP-473EBOO>
2024-06-12 16:33:11 -0600

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -1,3 +1,11 @@
+import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
+root = tk.TK()
+
+button1 = ttk.Button(root, text="button 1", bootsyle=SUCCESS)
+button1.pack(side=LEFT, padx=5, pady=10)
+
+button2 = ttk.Button(root, text="button 2", bootsyle=INFO)
+button2.pack(side=RIGHT, padx=5, pady=10)
