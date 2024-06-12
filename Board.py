6a9432aae3e4f896fb26e45b2e7eb1bb980923fe
root <root@DESKTOP-473EBOO>
2024-06-12 16:36:56 -0600

    Update Board.py

--- Board.py
+++ Board.py
@@ -24,3 +24,20 @@ class Board(Tk):
             for c in range(self.board_size):
                 self.button_board[r][c] = TicTacButton(r, c)
         self.button_board[r][c].grid(row=r, column=c)
+
+
+
+class TicTacButton:
+    def __init__(self, row, column):
+
+        self.row = row
+        self.column = column
+        self.button = Button(
+            height=2,
+            width=4,
+            font=("Ariel", "20"),
+            command=lambda r=self.row, c=self.column: self.clicked(r, c),
+        )
+
+    def clicked(r, c):
+        print(f"{r} + {c}")
