03c6fe788368565b5d4709557b6f7653cb36a4ca
root <root@DESKTOP-473EBOO>
2024-06-12 16:33:43 -0600

    Update mainOOP.py

--- mainOOP.py
+++ mainOOP.py
@@ -4,8 +4,8 @@ from ttkbootstrap.constants import *
 
 root = tk.Tk()
 
-button1 = ttk.Button(root, text="button 1", bootsyle=SUCCESS)
+button1 = ttk.Button(root, text="button 1", bootstyle=SUCCESS)
 button1.pack(side=LEFT, padx=5, pady=10)
 
-button2 = ttk.Button(root, text="button 2", bootsyle=INFO)
+button2 = ttk.Button(root, text="button 2", bootstyle=INFO)
 button2.pack(side=RIGHT, padx=5, pady=10)
